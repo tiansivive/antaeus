fb0bb9d35103686116468464f008b3a55394418f
Tiago Vila Verde <tiago.vilaverde@neotechnology.com>
2019-05-21 18:48:14 +0200

    add function to attempt billing

--- pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
@@ -1,9 +1,30 @@
 package io.pleo.antaeus.core.services
 
+
 import io.pleo.antaeus.core.external.PaymentProvider
 
+import io.pleo.antaeus.data.AntaeusDal
+import io.pleo.antaeus.models.Invoice
+import java.lang.Exception
+
 class BillingService(
-    private val paymentProvider: PaymentProvider
+		private val paymentProvider: PaymentProvider
 ) {
-   // TODO - Add code e.g. here
+
+	// TODO - Add code e.g. here
+
+	private fun attemptCharging(invoice: Invoice): Boolean{
+
+		return try {
+			this.paymentProvider.charge(invoice)
+		}catch (e: Exception){
+			false
+		}
+	}
+
+	fun bill(dal: AntaeusDal) : List<Boolean>{
+		return InvoiceService(dal)
+				.fetchAll()
+				.map { attemptCharging(it) }
+	}
 }
---
+++ pleo-antaeus-core/src/test/kotlin/io/pleo/antaeus/core/services/BillingServiceTest.kt
@@ -0,0 +1,76 @@
+package io.pleo.antaeus.core.services;
+
+
+import kotlin.random.Random
+
+import io.mockk.every
+import io.mockk.mockk
+import io.pleo.antaeus.core.exceptions.CustomerNotFoundException
+import io.pleo.antaeus.core.exceptions.CurrencyMismatchException
+import io.pleo.antaeus.core.exceptions.NetworkException
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.assertEquals
+
+import io.pleo.antaeus.core.external.PaymentProvider
+import io.pleo.antaeus.data.AntaeusDal
+import io.pleo.antaeus.models.*
+
+class BillingServiceTest
+{
+    private val nInvoices = 10
+    private val invoice = mockk<Invoice> ()
+
+    private val dal = mockk<AntaeusDal> {
+        every { fetchInvoices() } returns List( nInvoices) { invoice }
+    }
+
+
+    @Test
+    fun `will charge all invoices`() {
+
+        val paymentProvider = mockk< PaymentProvider > {
+            every { charge(invoice) } returns Random.nextBoolean()
+        }
+
+        val results = BillingService(paymentProvider).bill(dal)
+        assertEquals(nInvoices, results.size)
+    }
+
+
+    @Test
+    fun `will handle CustomerNotFoundException`() {
+
+        val paymentProvider = mockk< PaymentProvider > {
+            every { charge(invoice) } throws CustomerNotFoundException(0)
+        }
+
+        val results = BillingService(paymentProvider).bill(dal)
+        assertEquals(nInvoices, results.size)
+    }
+
+
+    @Test
+    fun `will handle CurrencyMismatchException`() {
+
+        val paymentProvider = mockk< PaymentProvider > {
+            every { charge(invoice) } throws CurrencyMismatchException(0, 0)
+        }
+
+        val results = BillingService(paymentProvider).bill(dal)
+        assertEquals(nInvoices, results.size)
+    }
+
+
+    @Test
+    fun `will handle NetworkException`() {
+
+        val paymentProvider = mockk< PaymentProvider > {
+            every { charge(invoice) } throws NetworkException()
+        }
+
+        val results = BillingService(paymentProvider).bill(dal)
+        assertEquals(nInvoices, results.size)
+    }
+}
+
